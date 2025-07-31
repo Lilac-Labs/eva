26ea97f74543817996f5a505358aa481e3c30d93
Shelden Shi <sheldenshi@gmail.com>
2025-07-30 22:29:04 -0700

    bug(eval): dummy test npm publish

--- apps/eval/src/eval/index.ts
+++ apps/eval/src/eval/index.ts
@@ -17,6 +17,7 @@ import { Presets, SingleBar } from 'cli-progress';
 import pLimit from 'p-limit';
 
 import type { BaseScore, DataItem, EvalConfig } from './eval.types';
+
 export class Eval<Input, Expected, Output, Score extends BaseScore> {
   readonly #dataProvider: () =>
     | Array<DataItem<Input, Expected>>
